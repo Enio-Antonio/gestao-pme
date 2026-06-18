0a1f3121cf10b0b6c7c7be74a65fe1d5aeab5a48
Francisco josé <149014180+Franciscojs01@users.noreply.github.com>
2026-06-18 19:45:24 -0300

    feat: adicionando models

--- apps/api/users/models.py
+++ apps/api/users/models.py
@@ -3,14 +3,20 @@ from django.db import models
 
 
 class User(AbstractUser):
-    ROLE_CHOICES = (
-        ("admin", "Empresa (Admin)"),
-        ("employee", "Funcionário (Usuário)"),
-    )
+  ROLE_CHOICES = {
+    "admin": "Empresa (Admin)",
+    "employee": "Funcionário (Usuário)",
+  }
 
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="employee")
-    company_name = models.CharField(max_length=255, blank=True, null=True)
-    company_code = models.CharField(max_length=100, blank=True, null=True)
+  role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="employee")
 
-    def __str__(self):
-        return self.email or self.username
+  empresa = models.ForeignKey(
+    'gestao.Empresa',
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    related_name='funcionarios'
+  )
+
+  def __str__(self):
+    return self.email or self.username
